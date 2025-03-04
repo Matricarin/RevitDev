247df865ade90f460f29597ecc49cdf367b8fe96
Matricarin <matricarin@gmail.com>
2025-03-04 21:54:23 +0500

    simply collecting was implemented

--- Plugins/GetWallsPlugin/Plugin.cs
+++ Plugins/GetWallsPlugin/Plugin.cs
@@ -1,14 +1,22 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using System;
+using System.Linq;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 namespace GetWallsPlugin;
 
 [Transaction(TransactionMode.Manual)]
+[Regeneration(RegenerationOption.Manual)]
 public class Plugin : IExternalCommand
 {
     public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
-        throw new System.NotImplementedException();
+        var uiDoc = commandData.Application;
+        var doc = uiDoc.ActiveUIDocument.Document;
+        var elementCollector = new FilteredElementCollector(doc);
+        var allWalls = elementCollector.OfClass(typeof(Wall)).WhereElementIsNotElementType().ToElements();
+        TaskDialog.Show("Walls: ",string.Join(Environment.NewLine, allWalls.Select(w => w.Name)) );
+        return Result.Succeeded;
     }
 }
