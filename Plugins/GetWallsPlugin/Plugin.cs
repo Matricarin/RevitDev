11d44230ed6fd4f699a9d5625ce448717cdb0861
Matricarin <matricarin@gmail.com>
2025-03-04 20:09:33 +0500

    new plugin initial commit

---
+++ Plugins/GetWallsPlugin/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace GetWallsPlugin;
+
+[Transaction(TransactionMode.Manual)]
+public class Plugin : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        throw new System.NotImplementedException();
+    }
+}
