32d7d1ccb5856158f0df8c2d0b435c20b1f8b7b7
Matricarin <matricarin@gmail.com>
2025-03-04 20:14:50 +0500

    wall model was added

---
+++ Plugins/GetWallsPlugin/Model/AtomWall.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.Revit.DB;
+
+namespace GetWallsPlugin.Model;
+
+public class AtomWall
+{
+    public ElementId Id { get; }
+    public string Name { get; }
+
+    public AtomWall(ElementId id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
