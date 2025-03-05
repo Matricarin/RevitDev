f9a39bbf5638e96939868e507bbd963c361e2aac
Matricarin <matricarin@gmail.com>
2025-03-05 21:14:10 +0500

    provider was added

---
+++ Plugins/GetWallsPlugin/Providers/AtomWallsProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using GetWallsPlugin.Extensions;
+using GetWallsPlugin.Model;
+
+namespace GetWallsPlugin.Providers;
+
+public class AtomWallsProvider
+{
+    private readonly FilteredElementCollector _collector;
+    public AtomWallsProvider(Document? document)
+    {
+        if (document.IsNull())
+        {
+            throw new NullReferenceException();
+        }
+
+        _collector = new FilteredElementCollector(document);
+    }
+    
+    public IEnumerable<AtomWall> GetAtomWalls()
+    {
+        using var enumerator = _collector.OfClass(typeof(Wall)).GetEnumerator();
+        return enumerator.ToIEnumerable<AtomWall>();
+    }
+}
